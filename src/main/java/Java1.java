947356ca493b1502bbdc4e6d8b6fe5fb57cb744c
Jerry Lee <oldratlee@gmail.com>
2015-08-22 21:59:59 +0800

    first commit, create scala project

---
+++ src/main/java/Java1.java
@@ -0,0 +1,8 @@
+/**
+ * @author Jerry Lee
+ */
+public class Java1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/scala/Scala1.scala
@@ -0,0 +1,8 @@
+/**
+ * @author Jerry Lee
+ */
+object Scala1 {
+  def main(args: Array[String]) {
+    Java1.main(new Array[String](0))
+  }
+}
